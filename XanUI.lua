4527dda806751c089681cb902871ad2b38b3ed1b
Xruptor <xruptor@manaflux.com>
2016-08-14 09:36:17 -0400

    Add quest levels to log/gossip and quest tracker -Why isn't this an option in the game :(

--- XanUI.lua
+++ XanUI.lua
@@ -341,6 +341,102 @@ end)
 	-- end
 -- end
 
+
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+----------------------------------------------------------------
+
+hooksecurefunc('QuestInfo_Display', function(template, parentFrame, acceptButton, material, mapView)
+	local elementsTable = template.elements
+	for i = 1, #elementsTable, 3 do
+		if elementsTable[i] == QuestInfo_ShowTitle then
+			if QuestInfoFrame.questLog then
+				local questLogIndex = GetQuestLogSelection()
+				local level = select(2, GetQuestLogTitle(questLogIndex))
+				local newTitle = "["..level.."] "..QuestInfoTitleHeader:GetText()
+				QuestInfoTitleHeader:SetText(newTitle)
+			end
+		end
+	end
+end)
+
+hooksecurefunc("QuestLogQuests_Update", function(self, pIndex)
+	local numEntries, numQuests = GetNumQuestLogEntries()
+	local headerCollapsed = false
+	local titleIndex = 0
+	for questLogIndex = 1, numEntries do
+		local title, level, suggestedGroup, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory = GetQuestLogTitle(questLogIndex)
+		if ( isHeader ) then
+			headerCollapsed = isCollapsed
+		elseif not isTask and (not isBounty or IsQuestComplete(questID)) and not headerCollapsed then
+			titleIndex = titleIndex + 1
+			local button = QuestLogQuests_GetTitleButton(titleIndex)
+			local oldBlockHeight = button:GetHeight()
+			local oldHeight = button.Text:GetStringHeight()
+			local newTitle = "["..level.."] "..button.Text:GetText()
+			button.Text:SetText(newTitle)
+			local newHeight = button.Text:GetStringHeight()
+			button:SetHeight(oldBlockHeight + newHeight - oldHeight)
+		end
+	end
+end)
+
+hooksecurefunc("GossipFrameUpdate", function(self)
+	local buttonIndex = 1
+
+	local availableQuests = {GetGossipAvailableQuests()}
+	local numAvailableQuests = table.getn(availableQuests)
+	for i=1, numAvailableQuests, 7 do
+		local titleButton = _G["GossipTitleButton" .. buttonIndex]
+		local title = "["..availableQuests[i+1].."] "..availableQuests[i]
+		local isTrivial = availableQuests[i+2]
+		if isTrivial then
+			titleButton:SetFormattedText(TRIVIAL_QUEST_DISPLAY, title)
+		else
+			titleButton:SetFormattedText(NORMAL_QUEST_DISPLAY, title)
+		end
+		GossipResize(titleButton)
+		buttonIndex = buttonIndex + 1
+	end
+	if numAvailableQuests > 1 then
+		buttonIndex = buttonIndex + 1
+	end
+
+	local activeQuests = {GetGossipActiveQuests()}
+	local numActiveQuests = table.getn(activeQuests)
+	for i=1, numActiveQuests, 6 do
+		local titleButton = _G["GossipTitleButton" .. buttonIndex]
+		local title = "["..activeQuests[i+1].."] "..activeQuests[i]
+		local isTrivial = activeQuests[i+2]
+		if isTrivial then
+			titleButton:SetFormattedText(TRIVIAL_QUEST_DISPLAY, title)
+		else
+			titleButton:SetFormattedText(NORMAL_QUEST_DISPLAY, title)
+		end
+		GossipResize(titleButton)
+		buttonIndex = buttonIndex + 1
+	end
+end)
+
+hooksecurefunc(QUEST_TRACKER_MODULE, "Update", function(self)
+	for i = 1, GetNumQuestWatches() do
+		local questID, title, questLogIndex, numObjectives, requiredMoney, isComplete, startEvent, isAutoComplete, failureTime, timeElapsed, questType, isTask, isStory, isOnMap, hasLocalPOI = GetQuestWatchInfo(i)
+		if ( not questID ) then
+			break
+		end
+		local oldBlock = QUEST_TRACKER_MODULE:GetExistingBlock(questID)
+		if oldBlock then
+			local oldBlockHeight = oldBlock.height
+			local oldHeight = QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, title, nil, OBJECTIVE_TRACKER_COLOR["Header"])
+			local newTitle = "["..select(2, GetQuestLogTitle(questLogIndex)).."] "..title
+			local newHeight = QUEST_TRACKER_MODULE:SetStringText(oldBlock.HeaderText, newTitle, nil, OBJECTIVE_TRACKER_COLOR["Header"])
+			oldBlock:SetHeight(oldBlockHeight + newHeight - oldHeight);
+		end
+	end
+end)
+
+
 ----------------------------------------------------------------
 ----------------------------------------------------------------
 ----------------------------------------------------------------

88f69771712f619977f1a46dc44d991bee24864d
Xruptor <xruptor@manaflux.com>
2020-02-07 15:54:59 -0500

    -Updated to a new way and more friendlier way to sell grey items at the merchant.  It should now wait for server responses instead of spamming it.

--- XanUI.lua
+++ XanUI.lua
@@ -6,6 +6,7 @@
 ----------------------------------------------------------------
 ---COLOR BARS BY CLASS
 ----------------------------------------------------------------
+local LibAceTimer = LibStub('AceTimer-3.0')
 
 local debugf = tekDebug and tekDebug:GetFrame("xanUI")
 local function Debug(...)
@@ -814,53 +815,97 @@ end
 eventFrame:RegisterEvent("MERCHANT_SHOW")
 
 local doGuildRepairs = false
+eventFrame.GreyLootList = {}
+eventFrame.moneyCount = 0
+eventFrame.itemCount = 0
+eventFrame.sellAttempts = 0
+eventFrame.totalSlots = 0
+
+local ignoreList = {
+	[140662] = "Deformed Eredar Head", --warlock artifact quest
+	[140661] = "Damaged Eredar Head", --warlock artifact quest
+	[140663] = "Malformed Eredar Head", --warlock artifact quest
+	[140664] = "Deficient Eredar Head", --warlock artifact quest
+	[140665] = "Nearly Satisfactory Eredar Head", --warlock artifact quest
+	--these two items for some reason break the selling
+	------
+	[158178] = "Mangled Tortollan Scroll", -- mangled-tortollan-scroll
+	--[167873] = "Remnant of the Void", -- remnant-of-the-void
+	------
+}
+
+function eventFrame:StopSellingTimer(endedEarly)
+	LibAceTimer:CancelTimer(eventFrame.sellGreyTimer)
+	eventFrame.sellGreyTimer = nil
+	if endedEarly then
+		DEFAULT_CHAT_FRAME:AddMessage("xanUI: (WARNING) you exited merchant before addon could finish selling greys.")
+	end
+
+	if eventFrame.moneyCount > 0 then
+		DEFAULT_CHAT_FRAME:AddMessage("xanUI: <"..eventFrame.itemCount.."> Total grey items vendored. ["..GetCoinTextureString(eventFrame.moneyCount).."]")
+	end		
+end
 
 function eventFrame:MERCHANT_SHOW()
 	
-	local moneyCount = 0
-	local itemCount = 0
+	--reset our variables
+	eventFrame.moneyCount = 0
+	eventFrame.itemCount = 0
+	eventFrame.GreyLootList = {}
+	eventFrame.totalSlots = 0
+	eventFrame.sellIndexCount = 0
 	
-	for bag = 0, 4, 1 do
-		for slot = 1, GetContainerNumSlots(bag), 1 do
+	-- gather info
+	for bag = 0, NUM_BAG_SLOTS do
+		for slot = 1, GetContainerNumSlots(bag) do
 			local itemID = GetContainerItemID(bag, slot)
-			if itemID then
-				local _, link, rarity, _, _, itype, _, _, _, _, itemPrice = GetItemInfo(itemID)
-				local stackCount = select(2, GetContainerItemInfo(bag, slot)) or 1
-
-				if (rarity and rarity == 0) and (itype and itype ~= "Quest") then
-				
-					if itemPrice and itemPrice > 0 then
-						local stackPrice = (itemPrice or 0) * stackCount
-						moneyCount = moneyCount + stackPrice
-						UseContainerItem(bag, slot)
-						itemCount = itemCount + 1
-					else
-						--it's garbage and has no value, so delete it
-						PickupContainerItem(bag, slot)
-						DeleteCursorItem()
+			if itemID and not ignoreList[itemID] then
+				local _, stackCount, _, quality, _, _, itemLink, _, noValue = GetContainerItemInfo(bag, slot)
+				if quality == 0 and not noValue then
+					local _, _, _, _, _, itemType, _, _, _, _, itemSellPrice = GetItemInfo(itemID)
+					--make sure it's not a quest item and it has a sell value
+					if itemType ~= "Quest" and quality == 0 and itemSellPrice > 0 then
+						local stackPrice = (itemSellPrice or 0) * stackCount
+						table.insert(eventFrame.GreyLootList, {bag=bag, slot=slot, itemID=itemID, stackCount=stackCount, stackPrice=stackPrice, itemSellPrice=itemSellPrice} )
 					end
 				end
-				
 			end
+			eventFrame.totalSlots = eventFrame.totalSlots + 1
 		end
 	end
-
-	-- local ignoreList = {
-		-- [140662] = "Deformed Eredar Head", --warlock artifact quest
-		-- [140661] = "Damaged Eredar Head", --warlock artifact quest
-		-- [140663] = "Malformed Eredar Head", --warlock artifact quest
-		-- [140664] = "Deficient Eredar Head", --warlock artifact quest
-		-- [140665] = "Nearly Satisfactory Eredar Head", --warlock artifact quest
-	-- }
-
-	-- result = tonumber(result:match("^(%d+):")) --just grab the first number
-	-- if result and ignoreList[result] then
-		-- ShowMerchantSellCursor(0)
-		-- return
-	-- else
-
-	if moneyCount > 0 then
-		DEFAULT_CHAT_FRAME:AddMessage("xanUI: <"..itemCount.."> Total items vendored. ["..GetCoinTextureString(moneyCount).."]")
+	
+	--do the timer for selling, only if we have something to work with
+	if #eventFrame.GreyLootList > 0 and not eventFrame.sellGreyTimer then
+		eventFrame.sellGreyTimer = LibAceTimer:ScheduleRepeatingTimer(function()
+			
+			--if they closed early then send a warning
+			if not MerchantFrame:IsVisible() then eventFrame:StopSellingTimer(true) end
+			
+			--if our attempts are more than wants on the list or more than our total amount of slots then exit
+			if eventFrame.sellIndexCount > #eventFrame.GreyLootList or eventFrame.sellIndexCount >= eventFrame.totalSlots or eventFrame.sellIndexCount > 2000 then
+				eventFrame:StopSellingTimer()
+				return
+			end
+			
+			eventFrame.sellIndexCount = eventFrame.sellIndexCount + 1
+			
+			--grab the next entry in our table
+			if eventFrame.GreyLootList[eventFrame.sellIndexCount] then
+			
+				local index = eventFrame.sellIndexCount
+				--print(index, eventFrame.GreyLootList[index].bag, eventFrame.GreyLootList[index].slot, eventFrame.GreyLootList[index].itemID, eventFrame.GreyLootList[index].stackCount, eventFrame.GreyLootList[index].stackPrice, eventFrame.GreyLootList[index].itemSellPrice)
+				
+				eventFrame.moneyCount = eventFrame.moneyCount + eventFrame.GreyLootList[index].stackPrice
+				eventFrame.itemCount = eventFrame.itemCount + 1
+				
+				--print(eventFrame.itemCount , eventFrame.moneyCount)
+				UseContainerItem(eventFrame.GreyLootList[index].bag, eventFrame.GreyLootList[index].slot)	
+			else
+				--we don't have another index so exit the timer
+				eventFrame:StopSellingTimer()
+			end
+		
+		end, 0.15)
 	end
 	
 	--do repairs
